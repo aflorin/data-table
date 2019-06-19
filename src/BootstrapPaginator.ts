85e3a1b856347ab4e96c7b74d8db756b69e15814
aflorin <florin.andriiciuc@assist.ro>
2019-06-19 14:39:15 +0300

    update

--- src/BootstrapPaginator.ts
+++ src/BootstrapPaginator.ts
@@ -5,68 +5,70 @@ import * as _ from 'lodash';
 @Component({
   selector: 'afBootstrapPaginator',
   template: `
-    <afPaginator #p [afTable]="afTable" *ngIf="p.dataLength > p.rowsOnPage">
-      <div>
-        Items per page:
-        <ul class="list-inline">
+    <afPaginator #p [afTable]="afTable">
+      <div class="paginator-container" *ngIf="p.dataLength > p.rowsOnPage">
+        <div>
+          Items per page:
+          <ul class="list-inline">
+            <li
+              class="list-inline-item"
+              *ngFor="let rows of rowsOnPageSet"
+              [class.active]="p.rowsOnPage === rows"
+              (click)="p.setRowsOnPage(rows)"
+            >
+              <a style="cursor: pointer">{{ rows }}</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>Page: {{ p.activePage }} of {{ p.lastPage }}</div>
+        <ul class="pagination">
           <li
-            class="list-inline-item"
-            *ngFor="let rows of rowsOnPageSet"
-            [class.active]="p.rowsOnPage === rows"
-            (click)="p.setRowsOnPage(rows)"
+            class="page-item"
+            [class.disabled]="p.activePage <= 1"
+            (click)="p.setPage(1)"
           >
-            <a style="cursor: pointer">{{ rows }}</a>
+            <a
+              class="page-link"
+              style="cursor: pointer"
+              [innerHTML]="buttons.firstBtn"
+            ></a>
+          </li>
+          <li
+            class="page-item page-item--previous"
+            [ngClass]="{ disabled: p.activePage === 1 }"
+          >
+            <a
+              class="page-link"
+              (click)="p.setPage(p.activePage - 1)"
+              style="cursor: pointer"
+              [innerHTML]="buttons.previousBtn"
+            ></a>
+          </li>
+          <li
+            class="page-item page-item--next"
+            [ngClass]="{ disabled: p.activePage === p.lastPage }"
+          >
+            <a
+              class="page-link"
+              (click)="p.setPage(p.activePage + 1)"
+              style="cursor: pointer"
+              [innerHTML]="buttons.nextBtn"
+            ></a>
+          </li>
+          <li
+            class="page-item"
+            [class.disabled]="p.activePage >= p.lastPage"
+            (click)="p.setPage(p.lastPage)"
+          >
+            <a
+              class="page-link"
+              style="cursor: pointer"
+              [innerHTML]="buttons.lastBtn"
+            ></a>
           </li>
         </ul>
       </div>
-
-      <div>Page: {{ p.activePage }} of {{ p.lastPage }}</div>
-      <ul class="pagination">
-        <li
-          class="page-item"
-          [class.disabled]="p.activePage <= 1"
-          (click)="p.setPage(1)"
-        >
-          <a
-            class="page-link"
-            style="cursor: pointer"
-            [innerHTML]="buttons.firstBtn"
-          ></a>
-        </li>
-        <li
-          class="page-item page-item--previous"
-          [ngClass]="{ disabled: p.activePage === 1 }"
-        >
-          <a
-            class="page-link"
-            (click)="p.setPage(p.activePage - 1)"
-            style="cursor: pointer"
-            [innerHTML]="buttons.previousBtn"
-          ></a>
-        </li>
-        <li
-          class="page-item page-item--next"
-          [ngClass]="{ disabled: p.activePage === p.lastPage }"
-        >
-          <a
-            class="page-link"
-            (click)="p.setPage(p.activePage + 1)"
-            style="cursor: pointer"
-            [innerHTML]="buttons.nextBtn"
-          ></a>
-        </li>
-        <li
-          class="page-item"
-          [class.disabled]="p.activePage >= p.lastPage"
-          (click)="p.setPage(p.lastPage)"
-        >
-          <a
-            class="page-link"
-            style="cursor: pointer"
-            [innerHTML]="buttons.lastBtn"
-          ></a>
-        </li>
-      </ul>
     </afPaginator>
   `
 })
